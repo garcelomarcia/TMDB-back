5f2a8e6d15ef0ff5b7f03cfb72796dc5eaab8832
garcelomarcia <marcelogarcia91@gmail.com>
2023-06-26 11:04:07 -0600

    fixed comma on sequelize instance

--- api/db/index.js
+++ api/db/index.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD{
-  host: "db", // Use the service name "db" as the host
-  port: 5432,
-  dialect: "postgres",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: "db", // Use the service name "db" as the host
+    port: 5432,
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
