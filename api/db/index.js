6fdea778aba0575008a41d5cf0479af39c34acb8
garcelomarcia <marcelogarcia91@gmail.com>
2023-06-26 10:05:43 -0600

    fixed db host

--- api/db/index.js
+++ api/db/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("tmdb_auth", "user", "password", {
-  host: "db", // Use the service name "db" as the host
+  host: "localhost", // Use the service name "db" as the host
   port: 5432,
   dialect: "postgres",
   logging: false,
