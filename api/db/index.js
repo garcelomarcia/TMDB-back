7c9b0d2647a8f1a94438bb14faab35bb0c501bb0
garcelomarcia <marcelogarcia91@gmail.com>
2023-06-26 17:29:51 -0600

    sequelize SSL

--- api/db/index.js
+++ api/db/index.js
@@ -3,6 +3,12 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // This line allows you to connect to a database with a self-signed certificate
+    },
+  },
 });
 
 module.exports = sequelize;
